342a24591a62b8428d307f90edbf31ce8c801215
Erzhan060708 <156690085+Erzhan060708@users.noreply.github.com>
2026-06-28 19:15:06 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,64 +1,19 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
-from enum import Enum
-
-
-class PriorityEnum(str, Enum):
-    Low = "Low"
-    Medium = "Medium"
-    High = "High"
-
-
-class StatusEnum(str, Enum):
-    New = "New"
-    InProgress = "In Progress"
-    Done = "Done"
-
-
-class UserRegister(BaseModel):
-    email: EmailStr
-    password: str
-    username: str
-
-
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
-
+from pydantic import BaseModel, ConfigDict
 
 class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)  # orm_mode емес!
+    
     id: int
     username: str
-    email: EmailStr
+    email: str
     role: str
-    model_config = {"from_attributes": True}
-
-
-class TokenResponse(BaseModel):
-    access_token: str
-    token_type: str
-    user: UserResponse
-
-
-class TaskCreate(BaseModel):
-    title: str
-    description: str = ""
-    priority: PriorityEnum = PriorityEnum.Medium
-    user_id: int
-
-
-class TaskUpdate(BaseModel):
-    status: Optional[StatusEnum] = None
-    priority: Optional[PriorityEnum] = None
-    title: Optional[str] = None
-    description: Optional[str] = None
-
 
 class TaskResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
     id: int
     title: str
-    description: str
-    status: str
+    description: str | None
     priority: str
+    status: str
     user_id: int
-    model_config = {"from_attributes": True}
